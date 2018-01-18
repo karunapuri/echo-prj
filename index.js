bc04c61b5dcf87f00fbf2392da6f38169232e97e
karunapuri <karunapuri@users.noreply.github.com>
2018-01-18 18:52:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,29 +11,38 @@ restService.use(
   })
 );
 
-const nodemailer = require('nodemailer');
+var express = require('express');
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+var sgTransport = require('nodemailer-sendgrid-transport');
+var app = express();
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'purikaruna@gmail.com',
-    pass: 'waheguruji4@'
-  }
+app.use(express.static(__dirname + '/client'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Sendmail route
+app.post('/sendmail', function(req, res){
+    var options = {
+        auth: {
+            api_key: 'YOUR_SENDGRID_API_KEY'
+        }
+    }
+    var mailer = nodemailer.createTransport(sgTransport(options));
+    mailer.sendMail(req.body, function(error, info){
+        if(error){
+            res.status('401').json({err: info});
+        }else{
+            res.status('200').json({success: true});
+        }
+    });
 });
 
-var mailOptions = {
-  from: 'purikaruna@gmail.com',
-  to: 'karuna.puri@springernature.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
+// Start server
+var port = 8080, ip = "127.0.0.1";
+app.listen(port, ip, function() {
+  console.log('Express server listening on http://localhost:%d', port);
 });
 
 restService.use(bodyParser.json());
